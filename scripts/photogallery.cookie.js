99517b9584e97332746e18014d478ad133b0469c
Ionut Carst <icarst@carst.me>
2012-05-06 18:48:59 +0200

    building preloader

--- scripts/photogallery.cookie.js
+++ scripts/photogallery.cookie.js
@@ -2,7 +2,10 @@ CookieGallery.cookie = new cookie();
 
 function cookie(){
 	var _cookie = this,
-		cookieName = CookieGallery._settings.setCookieName;
+		cookieName = CookieGallery._settings.setCookieName,
+		c_start = document.cookie.indexOf(cookieName + "="),
+		c_end = document.cookie.indexOf(";" , c_start);
+
 	
 	//set cookies name|value|time
 	this.set = function(name, value, time){
@@ -16,117 +19,73 @@ function cookie(){
 		document.cookie = name + "= " + value + expires + "; path=/";
 		
 	}
-	
 	//get cookies 
 	this.get = function(name){
-		/*
-		if(document.cookie.length > 0 ){
-			
-			//get the start and end of your cookie precaution if the server has already multiple cookies
-			var c_start = document.cookie.indexOf(name + "="),
-				c_end = document.cookie.indexOf(";" , c_start);
-			
+		if(document.cookie.length > 0 || document.cookie != ''){
 			//check the CookieGallery by name
 			if(c_start != -1){
-				
-				c_start = c_start + name.length + 1;
-				
-				//console.log(c_end);
-				
-				if(c_end == -1) {
-					
-					c_end = document.cookie.length;
-					
-					//console.log(c_end);
-					
+				var getIndexes = _cookie.getCIndexes(c_start, c_end);
+				var cookieName = name + '=',
+					splitCookies = getIndexes.split(/,/),
+					i = 0,
+					c = [];
+				for(i; i < splitCookies.length; i++){
+					c.push(splitCookies[i]);
 				}
-				return unescape(document.cookie.substring(c_start, c_end));
-				
+				checkRequest = true;
+				return c;
+			}else{
+				return 'cookies gal its not set';
 			}
-			
-			var cName = name + '=',
-				splitCookies = document.cookie.split(/;/),
-				i = 0,
-				c = [];
-			
-		}		
-		/*
-		var cookieName = name + '=',
-			splitCookies = document.cookie.split(/,|=|CookieGallery/),
-			i = 0,
-			c = [];
-			
-			
-			
-			
-		console.log(splitCookies);*/
-		/*
-		for(i; i < splitCookies.length; i++){
-			c.push(splitCookies[i]);
+		}else{
+			return 'cookies are not set yet';
 		}
-		
-		console.log(c);
-		return c;*/
-		
 	}
-	//fn for checking and setting the cookies with the images received from json
+	/*
+		if JS reading it's set build the second cookie for the thumbs
+		check if the cookies are set receive and if the thumbs cookie its not set set thumb cookie
+	*/
 	this.checkCookies = function(returnImg, returnThumb){
-		
-		
 		//if the thumbs img is false that means we have a php request to files
 		if(returnThumb != false){
-			var _returnImgs = returnThumb;
-			
-			
-			
-			
-			CookieGallery.cookie.set('CookieGalleryThumbs', _returnImgs, CookieGallery._settings.expireTime);
-			
-			
-			
+			var cName = 'CookieGalleryThumbs';
+			_cookie.checkCGal(returnThumb, cName);
+			CookieGallery.cookie.set(cName, returnThumb, CookieGallery._settings.expireTime);
 		}else{
-			var _returnImgs = returnImg;
+			//if its php request do the checking for cookie and set it
+			_cookie.checkCGal(returnImg, cookieName);
 		}
-		
-		
+		console.log('set cookoies');
+	}
+	/*
+		check the cookies are set
+		if the cookie its already set make sure it's the cookie gallery
+		if there are cookies but not the cookie gallery then set it
+	*/ 
+	this.checkCGal = function(_returnImgs, cName){
 		if(_returnImgs != ''){
-			
-
-			
-			
-		}
-		
-		this.checkCGal = function(){
-			
 			//if there are no cookies then set our cookie
-			if(document.cookie === '' || document.cookie.length < 0 ){
+			if(document.cookie === '' || document.cookie.length <= 0 ){
 				//set the cookies if there are not there
-				CookieGallery.cookie.set(cookieName, _returnImgs, CookieGallery._settings.expireTime);
-				
+				CookieGallery.cookie.set(cName, _returnImgs, CookieGallery._settings.expireTime);
 			}else{
-				
 				//get the start and end of your cookie appling expecialy of server has multiple cookies
-				var c_start = document.cookie.indexOf(cookieName + "="),
-					c_end = document.cookie.indexOf(";" , c_start);
-				//check if in cookies we find our cookie gallery
-				if(c_start != -1){
-					c_start = c_start + name.length + 1;
-
-					if(c_end == -1) {
-						c_end = document.cookie.length;
-					}
-					var substringCookie = unescape(document.cookie.substring(c_start, c_end));
-					console.log(substringCookie);
-					
-				}else{
-					//console.log('set our cokoie')
-					CookieGallery.cookie.set(cookieName, _returnImgs, CookieGallery._settings.expireTime);
-				}
-				
+				_cookie.getCIndexes(c_start, c_end, _returnImgs);
 			}			
-			
 		}
-		
+	}
+	//get the start and end of your cookie precaution if the ser,ver has already multiple cookies
+	this.getCIndexes = function(startVal, endVal, receivedImg){
+		//check if in cookies we find is the cookie gallery
+		if(startVal != -1){
+			startVal = startVal + cookieName.length + 1;
+			if(endVal == -1) {
+				endVal = document.cookie.length;
+			}
+			return unescape(document.cookie.substring(startVal, endVal));
+		}else{
+			CookieGallery.cookie.set(cookieName, receivedImg, CookieGallery._settings.expireTime);
+		}
 		
 	}
 	//clear cookies 
---
+++ scripts/photogallery.request.js
@@ -0,0 +1,106 @@
+//general fn for makeing the ajax request in the folders
+function httpRequest(xhr, path, filetype, splitArr){
+	var _xhr = xhr,
+		retrunImageFiles = [],
+		returnImageThumb = '',
+		count = 0,
+		total = 0,
+		sendUrl = mainObjSettings.readFiles + '?path=' + path; //url for read file server side
+	
+	if(window.XMLHttpRequest) {
+		_xhr = new XMLHttpRequest();
+	}else if(window.ActiveXObject) {
+		try{
+			_xhr = ActiveXObject("Msxml2.XMLHTTP.6.0");
+		}catch(e) {
+			try{
+				_xhr = ActiveXObject("Msxml2.XMLHTTP.3.0");
+			}catch(e) {
+				try{
+					_xhr = ActiveXObject("Microsoft.XMLHTTP");
+				}catch(e) {
+					console.log("This browser does not support XMLHttpRequest." + e);		
+				}
+			}
+		}	
+	}
+	if(_xhr){
+		_xhr.onreadystatechange = function(){
+			if(_xhr.readyState == 4){
+				if (_xhr.status == 200) {
+					
+					var responeTxt = _xhr.responseText,
+						matchExtension = responeTxt.match(filetype);
+
+					//check for images extenstions and if the response it's bigger then 0	
+					if(matchExtension && responeTxt.length > 0 && responeTxt.length != '' ){
+							
+						if(mainObjSettings.readFileType.rFServer === true){
+							
+							/* ---------------------------------
+							 * PHP JSON RESPONSE
+							 * --------------------------------- */
+							var parseResponse = JSON.parse(responeTxt);
+							
+							for(var x = 0; x < parseResponse.length; x++){
+								//check matches for extension from the json
+								if(parseResponse[x].match(filetype)){
+									//push images into the empty array
+									retrunImageFiles.push(parseResponse[x]);
+									console.log('load files')
+								}
+								
+							}
+							if(retrunImageFiles != ''){
+								CookieGallery.cookie.checkCookies(retrunImageFiles, false);
+								checkRequest = true;
+								console.log('received request');
+							}
+							
+						}else if(mainObjSettings.readFileType.rFClient === true){
+
+							/* -------------------------------
+							 * LOCAL HOST REQUEST AND PARSE
+							 *  ------------------------------ */ 
+							//remove the a tag so it wont get duplicated entries
+							var splitArray = responeTxt.split(splitArr);
+							total = splitArray.length;
+							
+							//loop through the array and get the founded files from dir
+							for(var x = 0; x < splitArray.length; x++){
+								count++;
+								//check matches for extension from the array
+								if(splitArray[x].match(filetype)){
+									//check for path and if it's thumb then add thumb to the small images
+									if(path === mainObjSettings.imagesdir){
+										retrunImageFiles += splitArray[x];
+									}
+									if(path === mainObjSettings.thumbdir){
+										returnImageThumb += 'thumb_' + splitArray[x] + ' ';
+									}
+								}
+							}
+							if(count == total){
+								CookieGallery.cookie.checkCookies(retrunImageFiles, returnImageThumb);
+							}
+						}else{
+							console.log('please make sure you have enabled one reading file option')
+						}
+					}else{
+						console.log('you don\'t have any images in folders');
+					}
+				}else{
+					console.log("Error Code:" + _xhr.status + ' Error Type:' + _xhr.statusText);  
+				}
+			}
+		}
+		if(mainObjSettings.readFileType.rFServer === true){
+			//php get requst
+			_xhr.open("GET", sendUrl, true);
+		}else if(mainObjSettings.readFileType.rFClient === true){
+			//localhost request returing 
+			_xhr.open("GET", path, true);
+		}
+		_xhr.send(null);
+	}
+}
